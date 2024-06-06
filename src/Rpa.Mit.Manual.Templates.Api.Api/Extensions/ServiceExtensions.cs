bad4daf8f9ec90919d21f0c9f62751db7e70fa3f
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-06-06 12:31:48 +0100

    removed automapper and placeholder services functionality

--- src/Rpa.Mit.Manual.Templates.Api.Api/Extensions/ServiceExtensions.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Api/Extensions/ServiceExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Rpa.Mit.Manual.Templates.Api.Api.Endpoints.Invoices;
 using Rpa.Mit.Manual.Templates.Api.Api.Endpoints.PaymentRequests;
 using Rpa.Mit.Manual.Templates.Api.Core.Interfaces;
-using Rpa.Mit.Manual.Templates.Api.Core.Services;
 
 namespace Rpa.Mit.Manual.Templates.Api.Api.Extensions;
 public static class ServiceExtensions
@@ -9,7 +8,6 @@ public static class ServiceExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddTransient<IInvoiceRepo, InvoiceRepo>();
-        services.AddTransient<IItemService, ItemService>();
         services.AddTransient<IPaymentRequestRepo, PaymentRequestRepo>();
         return services;
     }
