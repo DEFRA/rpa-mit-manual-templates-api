ba7d92912c405ebd322dad1ee181a8c900000b7a
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-06-10 14:52:23 +0100

    inc tests

--- test/Rpa.Mit.Manual.Templates.Api.Api.Tests/EndpointTests/InvoiceTests.cs
+++ test/Rpa.Mit.Manual.Templates.Api.Api.Tests/EndpointTests/InvoiceTests.cs
@@ -70,5 +70,31 @@ namespace Rpa.Mit.Manual.Templates.Api.Api.Tests.EndpointTests
             Assert.Equal("Error adding new invoice", response.Message);
             Assert.Null(response.Invoice);
         }
+
+        [Fact]
+        public async Task Invoice_Save_Returns_Exception()
+        {
+            AddInvoiceRequest invoiceRequest = new AddInvoiceRequest
+            {
+                SchemeType = "EU (2014 - 2020 Program)",
+                PaymentType = "GBP",
+                AccountType = "AP",
+                SecondaryQuestion = "Any question"
+            };
+
+            var fakeRepo = A.Fake<IInvoiceRepo>();
+            A.CallTo(() => fakeRepo.AddInvoice(A<Invoice>.Ignored, CancellationToken.None))
+                    .Throws<NullReferenceException>();
+
+            var ep = Factory.Create<AddInvoiceEndpoint>(
+                           A.Fake<ILogger<AddInvoiceEndpoint>>(),
+                           fakeRepo);
+
+            await ep.HandleAsync(invoiceRequest, default);
+            var response = ep.Response;
+
+            Assert.Equal("Object reference not set to an instance of an object.", response.Message);
+            Assert.Null(response.Invoice);
+        }
     }
 }
