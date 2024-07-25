38266c9041f38b3b83c655e8b8d1c49532b1a69e
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-07-25 11:57:04 +0100

    sonar!

--- src/Rpa.Mit.Manual.Templates.Api.Api/Program.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Api/Program.cs
@@ -6,57 +6,60 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 using Rpa.Mit.Manual.Templates.Api.Api;
 
-[ExcludeFromCodeCoverage]
-public partial class Program
+namespace GetSchemeTypes
 {
-    protected Program()
-    {}
-
-    private static async Task Main(string[] args)
+    [ExcludeFromCodeCoverage]
+    public partial class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
+        protected Program()
+        { }
 
-        builder.Services
-           .AddFastEndpoints()
-           .SwaggerDocument();
-
-        // Add services to the container.
-        ConfigureServices.Configure(builder);
-
-        builder.Services.ConfigureAzure(builder.Configuration);
-
-        // add this to support excel reader
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-        var app = builder.Build();
-
-        app.UseFastEndpoints()
-            .UseSwaggerGen();
-
-        app.UseAuthentication();
-
-        app.UseResponseCaching();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        private static async Task Main(string[] args)
         {
-            app.UseDeveloperExceptionPage();
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services
+               .AddFastEndpoints()
+               .SwaggerDocument();
+
+            // Add services to the container.
+            ConfigureServices.Configure(builder);
+
+            builder.Services.ConfigureAzure(builder.Configuration);
+
+            // add this to support excel reader
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var app = builder.Build();
+
+            app.UseFastEndpoints()
+                .UseSwaggerGen();
+
+            app.UseAuthentication();
+
+            app.UseResponseCaching();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseExceptionHandler();
+
+            app.UseHttpsRedirection();
+
+
+            app.UseHealthChecks("/healthy", new HealthCheckOptions()
+            {
+                Predicate = check => check.Name == "ready"
+            });
+            app.UseHealthChecks("/healthz", new HealthCheckOptions()
+            {
+                Predicate = check => check.Name == "live"
+            });
+
+            await app.RunAsync();
         }
-
-        app.UseExceptionHandler();
-
-        app.UseHttpsRedirection();
-
-
-        app.UseHealthChecks("/healthy", new HealthCheckOptions()
-        {
-            Predicate = check => check.Name == "ready"
-        });
-        app.UseHealthChecks("/healthz", new HealthCheckOptions()
-        {
-            Predicate = check => check.Name == "live"
-        });
-
-        await app.RunAsync();
     }
 }
