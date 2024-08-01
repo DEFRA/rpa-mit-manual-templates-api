b4fc6fb1b69a6de1e264545a828e52c02592eb50
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-08-01 16:35:37 +0100

    sonar!

--- src/Rpa.Mit.Manual.Templates.Api.Api/Endpoints/InvoiceLines/Update/Models.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Api/Endpoints/InvoiceLines/Update/Models.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-using InvoiceLines.Add;
 
 using Rpa.Mit.Manual.Templates.Api.Core.Entities;
 
@@ -18,15 +17,15 @@ namespace InvoiceLines.Update
 
         public string Description { get; set; } = string.Empty;
 
+        public string MarketingYear { get; set; } = string.Empty;
+
+        public string DeliveryBody { get; set; } = string.Empty;
+
         public string FundCode { get; set; } = string.Empty;
 
         public string MainAccount { get; set; } = string.Empty;
 
         public string SchemeCode { get; set; } = string.Empty;
-
-        public string MarketingYear { get; set; } = string.Empty;
-
-        public string DeliveryBody { get; set; } = string.Empty;
     }
 
     [ExcludeFromCodeCoverage]
@@ -41,6 +40,10 @@ namespace InvoiceLines.Update
             RuleFor(x => x.Value)
                 .GreaterThan(0).WithMessage("Value must be greater than 0!");
 
+            RuleFor(x => x.SchemeCode)
+                .NotEmpty().WithMessage("SchemeCode is required!")
+                .MinimumLength(3).WithMessage("SchemeCode is too short!");
+
             RuleFor(x => x.FundCode)
                 .NotEmpty().WithMessage("FundCode is required!")
                 .MinimumLength(3).WithMessage("FundCode is too short!")
@@ -48,10 +51,6 @@ namespace InvoiceLines.Update
 
             RuleFor(x => x.MainAccount)
                 .NotEmpty().WithMessage("MainAccount is required!");
-
-            RuleFor(x => x.SchemeCode)
-                .NotEmpty().WithMessage("SchemeCode is required!")
-                .MinimumLength(3).WithMessage("SchemeCode is too short!");
 
             RuleFor(x => x.MarketingYear)
                 .NotEmpty().WithMessage("MarketingYear is required!")
