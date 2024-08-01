d86e3e040478c70735a390ae06add5dcc5bd5882
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-08-01 16:41:31 +0100

    sonar! p..o..

--- src/Rpa.Mit.Manual.Templates.Api.Core/Entities/InvoiceLine.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Core/Entities/InvoiceLine.cs
@@ -13,14 +13,14 @@ namespace Rpa.Mit.Manual.Templates.Api.Core.Entities
 
         public string Description { get; set; } = string.Empty;
 
+        public string MarketingYear { get; set; } = string.Empty;
+
+        public string DeliveryBody { get; set; } = string.Empty;
+
         public string FundCode { get; set; } = string.Empty;
 
         public string MainAccount { get; set; } = string.Empty;
 
         public string SchemeCode { get; set; } = string.Empty;
-
-        public string MarketingYear { get; set; } = string.Empty;
-
-        public string DeliveryBody { get; set; } = string.Empty;
     }
 }
