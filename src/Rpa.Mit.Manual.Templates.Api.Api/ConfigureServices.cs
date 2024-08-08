8cbedd6171ca14b75088fe83135ba818da4a4ca9
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-08-08 09:24:07 +0100

    some vars

--- src/Rpa.Mit.Manual.Templates.Api.Api/ConfigureServices.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Api/ConfigureServices.cs
@@ -29,7 +29,7 @@ namespace Rpa.Mit.Manual.Templates.Api.Api
 
             builder.Services.Configure<AzureAd>(builder.Configuration.GetSection("AzureAd"));
             //builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
-            builder.Services.Configure<GovNotify>(builder.Configuration.GetSection("GOVNOTYIFY"));
+            builder.Services.Configure<GovNotify>(builder.Configuration.GetSection("GOVNOTIFY"));
             builder.Services.Configure<PaymentHub>(builder.Configuration.GetSection("PAYMENTHUB"));
             builder.Services.Configure<PostGres>(builder.Configuration.GetSection("POSTGRES"));
             builder.Services.Configure<PaymentHub>(builder.Configuration.GetSection("PAYMENTHUB"));
