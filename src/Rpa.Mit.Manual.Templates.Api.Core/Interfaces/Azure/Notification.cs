cef5449c3bc8ecf97a74621f9317e5fa5709036a
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-09-02 16:54:54 +0100

    sonar

--- src/Rpa.Mit.Manual.Templates.Api.Core/Interfaces/Azure/Notification.cs
+++
@@ -1,6 +0,0 @@
-﻿using Rpa.Mit.Manual.Templates.Api.Core.Entities.Azure;
-
-namespace Rpa.Mit.Manual.Templates.Api.Core.Interfaces.Azure
-{
-    public sealed record Notification(PaymentHubResponseRoot paymentHubResponseRoot);
-}
