e85a8519a777dfda83fb1825ac82aec7faaa435c
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-09-24 10:45:16 +0100

    and again

--- src/Rpa.Mit.Manual.Templates.Api.Core/Entities/InvoiceAr.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Core/Entities/InvoiceAr.cs
@@ -1,5 +1,8 @@
-﻿namespace Rpa.Mit.Manual.Templates.Api.Core.Entities
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Rpa.Mit.Manual.Templates.Api.Core.Entities
 {
+    [ExcludeFromCodeCoverage]
     public sealed class InvoiceAr : InvoiceBase
     {
         public IEnumerable<InvoiceRequestAr> InvoiceRequests { get; set; } = Enumerable.Empty<InvoiceRequestAr>();
