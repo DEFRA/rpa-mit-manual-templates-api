ebdbc23361d34ad72e795b7429f5a60032ca631d
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-11-18 14:03:14 +0000

    my

--- src/Rpa.Mit.Manual.Templates.Api.Api/Endpoints/BulkUploads/ValidationService.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Api/Endpoints/BulkUploads/ValidationService.cs
@@ -22,9 +22,7 @@ namespace Rpa.Mit.Manual.Templates.Api.Api.Endpoints.BulkUploads
         }
 
         public async Task<bool> MarketingYearIsValid(IEnumerable<MarketingYear> marketingYears, string org, CancellationToken ct)
-        {
-            return await Task.Run(() => marketingYears.Any(p => p.Description.Contains(org)));
-        }
+            => await Task.Run(() => marketingYears.Any(p => p.Description.Contains(org)));
 
         public async Task<bool> MainAccountIsValid(IEnumerable<MainAccount> mainAccounts, string mainAccount, string org, CancellationToken ct)
             => await Task.Run(() => mainAccounts.Any(p => p.Code == mainAccount &&  p.Org == org));
