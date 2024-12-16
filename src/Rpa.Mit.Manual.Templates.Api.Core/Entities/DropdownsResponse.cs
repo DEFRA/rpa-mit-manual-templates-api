3dbae564176c2cfb60e617fcf350968b6fcaee02
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-12-16 11:25:11 +0000

    ssoonnaarr

--- src/Rpa.Mit.Manual.Templates.Api.Core/Entities/DropdownsResponse.cs
+++ src/Rpa.Mit.Manual.Templates.Api.Core/Entities/DropdownsResponse.cs
@@ -1,5 +1,8 @@
-﻿namespace Rpa.Mit.Manual.Templates.Api.Core.Entities
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Rpa.Mit.Manual.Templates.Api.Core.Entities
 {
+    [ExcludeFromCodeCoverage]
     public class DropdownsResponse
     {
         public string Funds { get; set; } = string.Empty;
