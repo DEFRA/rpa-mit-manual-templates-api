ab6624a27094a7fa3aeeb4ac5875c5a08ebefeea
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-12-17 16:40:15 +0000

    RPA/BK named range tests

---
+++ test/Rpa.Mit.Manual.Templates.Api.Api.Tests/NamedRangeTests/APNamedRangeTests_Org_RPA_DBody_BK.cs
@@ -0,0 +1,52 @@
+﻿namespace Rpa.Mit.Manual.Templates.Api.Api.Tests.NamedRangeTests
+{
+    /// <summary>
+    /// AP tests for the org=RPA, dBody=BK combination
+    /// </summary>
+    public class APNamedRangeTests_Org_RPA_DBody_BK : BaseAPTests
+    {
+        private readonly string _org = "RPA";
+        private readonly string _dBody = "DF";
+
+        [Fact]
+        public void FundCode()
+        {
+            var namedRange = _namedRangeService.GetFundCodeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("RPAFunds", namedRange);
+        }
+
+        [Fact]
+        public void MainAccount()
+        {
+            var namedRange = _namedRangeService.GetAccountNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("BKAPAccounts", namedRange);
+        }
+
+        [Fact]
+        public void Scheme()
+        {
+            var namedRange = _namedRangeService.GetSchemeTypeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("BKSchemes", namedRange);
+        }
+
+        [Fact]
+        public void MarketingYear()
+        {
+            var namedRange = _namedRangeService.GetMarketingYearNamedRange(_org, _dBody);
+
+            Assert.Equal("NSMY", namedRange);
+        }
+
+        [Fact]
+        public void DeliveryBody()
+        {
+            var namedRange = _namedRangeService.GetDeliveryBodyNamedRange(_org, _dBody);
+
+            Assert.Equal("RPADBs", namedRange);
+        }
+
+    }
+}
