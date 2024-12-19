cb6acfe7126d8fad19fd39e0622b0fd0cdb7f762
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-12-19 10:40:44 +0000

    FC/FCLS named range tests

---
+++ test/Rpa.Mit.Manual.Templates.Api.Api.Tests/NamedRangeTests/APNamedRangeTests_Org_FC_DBody_FCLS.cs
@@ -0,0 +1,48 @@
+﻿namespace Rpa.Mit.Manual.Templates.Api.Api.Tests.NamedRangeTests
+{
+    public class APNamedRangeTests_Org_FC_DBody_FCLS : BaseAPTests
+    {
+        private readonly string _org = "FC";
+        private readonly string _dBody = "FCLS";
+
+        [Fact]
+        public void FundCode()
+        {
+            var namedRange = _namedRangeService.GetFundCodeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("FCAP", namedRange);
+        }
+
+        [Fact]
+        public void MainAccount()
+        {
+            var namedRange = _namedRangeService.GetAccountNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("FCLSAPAccounts", namedRange);
+        }
+
+        [Fact]
+        public void Scheme()
+        {
+            var namedRange = _namedRangeService.GetSchemeTypeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("FCLSSchemes", namedRange);
+        }
+
+        [Fact]
+        public void MarketingYear()
+        {
+            var namedRange = _namedRangeService.GetMarketingYearNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("LSMY", namedRange);
+        }
+
+        [Fact]
+        public void DeliveryBody()
+        {
+            var namedRange = _namedRangeService.GetDeliveryBodyNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("FCDBs", namedRange);
+        }
+    }
+}
