0a2ce5a09b630884025966801462b722d91179d0
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-12-23 09:11:44 +0000

    RDPE/RDPECS named range tests

---
+++ test/Rpa.Mit.Manual.Templates.Api.Api.Tests/NamedRangeTests/APTests/Org_RDPE_DBody_RDPECS.cs
@@ -0,0 +1,48 @@
+﻿namespace Rpa.Mit.Manual.Templates.Api.Api.Tests.NamedRangeTests.APTests
+{
+    public class Org_RDPE_DBody_RDPECS : BaseAPTests
+    {
+        private readonly string _org = "RDPE";
+        private readonly string _dBody = "RDPECS";
+
+        [Fact]
+        public void FundCode()
+        {
+            var namedRange = _namedRangeService.GetFundCodeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("NSFunds", namedRange);
+        }
+
+        [Fact]
+        public void MainAccount()
+        {
+            var namedRange = _namedRangeService.GetAccountNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("RDPECSAPAccounts", namedRange);
+        }
+
+        [Fact]
+        public void Scheme()
+        {
+            var namedRange = _namedRangeService.GetSchemeTypeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("RDPECSSchemes", namedRange);
+        }
+
+        [Fact]
+        public void MarketingYear()
+        {
+            var namedRange = _namedRangeService.GetMarketingYearNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("NAMY", namedRange);
+        }
+
+        [Fact]
+        public void DeliveryBody()
+        {
+            var namedRange = _namedRangeService.GetDeliveryBodyNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("RDPEDBs", namedRange);
+        }
+    }
+}
