4424dd325061673ab84bb4dc63438e2d0cfbc256
Aylmer Carson <aylmer.carson.external@eviden.com>
2024-12-23 10:03:27 +0000

    AR/EA/EA named range tests

---
+++ test/Rpa.Mit.Manual.Templates.Api.Api.Tests/NamedRangeTests/ARTests/Org_EA_DBody_EA.cs
@@ -0,0 +1,48 @@
+﻿namespace Rpa.Mit.Manual.Templates.Api.Api.Tests.NamedRangeTests.ARTests
+{
+    public class Org_EA_DBody_EA : BaseARTests
+    {
+        private readonly string _org = "EA";
+        private readonly string _dBody = "EA";
+
+        [Fact]
+        public void FundCode()
+        {
+            var fundNamedRange = _namedRangeService.GetFundCodeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("EAFunds", fundNamedRange);
+        }
+
+        [Fact]
+        public void MainAccount()
+        {
+            var range = _namedRangeService.GetAccountNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("EAARAccounts", range);
+        }
+
+        [Fact]
+        public void Scheme()
+        {
+            var range = _namedRangeService.GetSchemeTypeNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("EASchemes", range);
+        }
+
+        [Fact]
+        public void MarketingYear()
+        {
+            var range = _namedRangeService.GetMarketingYearNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("NSMY", range);
+        }
+
+        [Fact]
+        public void DeliveryBody()
+        {
+            var range = _namedRangeService.GetDeliveryBodyNamedRange(_org, _dBody, _accountType);
+
+            Assert.Equal("EADBs", range);
+        }
+    }
+}
---
+++ test/Rpa.Mit.Manual.Templates.Api.Api.Tests/NamedRangeTests/BaseARTests.cs
@@ -0,0 +1,10 @@
+﻿using Rpa.Mit.Manual.Templates.Api.Api.Services;
+
+namespace Rpa.Mit.Manual.Templates.Api.Api.Tests.NamedRangeTests
+{
+    public abstract class BaseARTests
+    {
+        public readonly string _accountType = "AR";
+        public readonly NamedRangeService _namedRangeService = new NamedRangeService();
+    }
+}
